17f30661e5865a5a900b6e4218dcad1f57bda219
Benzi <benzi.villaruel@gmail.com>
2024-11-08 02:38:00 -0500

    Add units and tags loader functions

--- dataloader.py
+++ dataloader.py
@@ -35,3 +35,52 @@ class DataLoader:
             key, value = line.split('|', 1)
             localization[key.strip()] = value.strip()
         return localization
+
+    def load_data(self):
+        gameData = self.comlink.get_game_data()
+        localization = self.get_localization()
+        self.load_units(gameData['units'], localization)
+        self.load_tags(gameData['category'], localization)
+
+    def load_units(self, units, localization):
+        for unit in units:
+            if unit['obtainableTime'] == '0':
+                baseId = unit['baseId']
+                name = localization[unit['nameKey']]
+                desc = localization[unit['descKey']]
+                query = '''
+                INSERT INTO units (baseId, name, description) VALUES (%s, %s, %s)
+                ON CONFLICT (baseId) DO UPDATE SET
+                name = excluded.name,
+                description = excluded.description;
+                '''
+                self.cursor.execute(query, (baseId, name, desc))
+                self.connection.commit()
+
+    def load_tags(self, tags, localization):
+        for tag in tags:
+            if (tag['visible']):
+                id = tag['id']
+                name = localization[tag['descKey']]
+                query = '''
+                INSERT INTO tags (id, name) VALUES (%s, %s)
+                ON CONFLICT (id) DO UPDATE SET
+                name = excluded.name;
+                '''
+                self.cursor.execute(query, (id, name))
+                self.connection.commit()
+
+    def load_unit_tags(self, units):
+        query = '''
+        INSERT INTO unit_tags (unitId, tagId) VALUES (%s, %s)
+        ON CONFLICT (unitId, tagId) DO NOTHING'''
+        self.cursor.exeute("SELECT id from tags")
+        visibleTags = {row[0] for row in self.cursor.fetchall()}
+        self.cursor.execute("SELECT baseId from units")
+        playableUnits = {row[0] for row in self.cursor.fetchall()}
+        for unit in units:
+            baseId = unit['baseId']
+            for tag in unit['categoryId']:
+                if tag in visibleTags:
+                    self.cursor.execute(query, (baseId, tag))
+        self.connection.commit()
