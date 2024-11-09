b51b9960db84ee77df85eab54a3f9a251969ab0d
Benzi <benzi.villaruel@gmail.com>
2024-11-09 16:36:49 -0500

    Optimize dataloader functions and improve readability

--- dataloader.py
+++ dataloader.py
@@ -37,98 +37,119 @@ class DataLoader:
         return localization
 
     def load_data(self):
-        gameData = self.comlink.get_game_data(include_pve_units=False)
-        localization = self.get_localization()
-        self.load_units(gameData['units'], localization)
-        self.load_tags(gameData['category'], localization)
-        self.load_abilities(gameData['units'], gameData['skill'], gameData['ability'], localization)
-    def load_units(self, units, localization):
-        for unit in units:
-            if unit['obtainableTime'] == '0':
-                baseId = unit['baseId']
-                name = localization[unit['nameKey']]
-                desc = localization[unit['descKey']]
-                query = '''
-                INSERT INTO units (unit_id, name, description) VALUES (%s, %s, %s)
-                ON CONFLICT (unit_id) DO UPDATE SET
-                name = excluded.name,
-                description = excluded.description;
-                '''
-                self.cursor.execute(query, (baseId, name, desc))
-                self.connection.commit()
-
-    def load_tags(self, tags, localization):
-        for tag in tags:
-            if (tag['visible']):
-                id = tag['id']
-                name = localization[tag['descKey']]
-                query = '''
-                INSERT INTO tags (tag_id, name) VALUES (%s, %s)
-                ON CONFLICT (tag_id) DO UPDATE SET
-                name = excluded.name;
-                '''
-                self.cursor.execute(query, (id, name))
-                self.connection.commit()
-
-    def load_unit_tags(self, units):
+        self.gameData = self.comlink.get_game_data(include_pve_units=False)
+        self.localization = self.get_localization()
+        self.load_units()
+        self.load_tags()
+        self.load_unit_tags()
+        self.load_abilities()
+    
+    def load_units(self):
         query = '''
-        INSERT INTO unit_tags (unit_id, tag_id) VALUES (%s, %s)
-        ON CONFLICT (unit_id, tag_id) DO NOTHING'''
+        INSERT INTO units (unit_id, name, description) VALUES (%s, %s, %s)
+        ON CONFLICT (unit_id) DO UPDATE SET
+        name = excluded.name,
+        description = excluded.description;
+        '''
+        processedUnits = set()
+
+        for unit in self.gameData['units']:
+            unitId = unit['baseId']
+            if unit['obtainableTime'] != '0' or unitId in processedUnits:
+                continue
+            
+            name = self.localization[unit['nameKey']]
+            desc = self.localization[unit['descKey']]
+            
+            self.cursor.execute(query, (unitId, name, desc))
+            processedUnits.add(unitId)
+
+        self.connection.commit()
+
+    def load_tags(self):
+        query = '''
+        INSERT INTO tags (tag_id, name) VALUES (%s, %s)
+        ON CONFLICT (tag_id) DO UPDATE SET
+        name = excluded.name;
+        '''
+
+        for tag in self.gameData['category']:
+            if not tag['visible']:
+                continue
+
+            id = tag['id']
+            name = self.localization[tag['descKey']]
+            self.cursor.execute(query, (id, name))
+
+        self.connection.commit()
+
+    def load_unit_tags(self):
         self.cursor.execute("SELECT tag_id from tags")
         visibleTags = {row[0] for row in self.cursor.fetchall()}
+
         self.cursor.execute("SELECT unit_id from units")
         playableUnits = {row[0] for row in self.cursor.fetchall()}
-        for unit in units:
-            baseId = unit['baseId']
-            if baseId in playableUnits:
-                for tag in unit['categoryId']:
-                    if tag in visibleTags:
-                        self.cursor.execute(query, (baseId, tag))
+
+        query = '''
+        INSERT INTO unit_tags (unit_id, tag_id) VALUES (%s, %s)
+        ON CONFLICT (unit_id, tag_id) DO NOTHING
+        '''
+        processedUnits = set()
+
+        for unit in self.gameData['units']:
+            unitId = unit['baseId']
+            if unitId not in playableUnits or unitId in processedUnits:
+                continue
+
+            for tag in unit['categoryId']:
+                if tag in visibleTags:
+                    self.cursor.execute(query, (unitId, tag))
+
+            processedUnits.add(unitId)
+
         self.connection.commit()
     
-    def load_abilities(self, units, skills, abilities, localization):
-        queryUnits = '''
+    def load_abilities(self):
+        selectUnits = '''
         SELECT unit_id FROM units
         '''
-        queryAbilities = '''
+        insertAbilities = '''
         INSERT INTO abilities (skill_id, name, description, max_level, is_zeta, is_omicron, omicron_mode) VALUES (%s, %s, %s, %s, %s, %s, %s)
         ON CONFLICT (skill_id) DO UPDATE SET
         description = excluded.description
         '''
-        queryUnitsAbilities = '''
+        insertUnitsAbilities = '''
         INSERT INTO unit_abilities (unit_id, ability_id) VALUES (%s, %s)
         ON CONFLICT (unit_id, ability_id) DO NOTHING
         '''
-        self.cursor.execute(queryUnits)
-        playableUnits = [row[0] for row in self.cursor.fetchall()]
-        for unit in units:
-            baseId = unit['baseId']
-            if baseId not in playableUnits:
+        self.cursor.execute(selectUnits)
+        playableUnits = {row[0] for row in self.cursor.fetchall()}
+
+        #Lookup dictionaries for helper function
+        skills = {s['id']: s for s in self.gameData['skill']}
+        abilities = {a['id']: a for a in self.gameData['ability']}
+
+        for unit in self.gameData['units']:
+            unitId = unit['baseId']
+            if unitId not in playableUnits:
                 continue
-            skillIds = [skill['skillId'] for skill in unit['skillReference']]
-            skillIds += [skill['skillReference'][0]['skillId'] for skill in unit['crew']]
+            skillIds = [s['skillId'] for s in unit['skillReference']] + [s['skillReference'][0]['skillId'] for s in unit['crew']]
             for id in skillIds:
-                name, desc, maxLevel, isZeta, isOmicron, omiMode = self.__get_skill_data(id, skills, abilities, localization)
-                self.cursor.execute(queryAbilities, (id, name, desc, maxLevel, isZeta, isOmicron, omiMode))
-                self.cursor.execute(queryUnitsAbilities, (baseId, id))
+                skillData = self.__get_skill_data(id, skills, abilities)
+                self.cursor.execute(insertAbilities, skillData)
+                self.cursor.execute(insertUnitsAbilities, (unitId, id))
+
         self.connection.commit()
             
-    def __get_skill_data(self, id, skills, abilities, localization):
-        for skill in skills:
-            if skill['id'] != id:
-                continue
-            maxLevel = len(skill['tier']) + 1
-            isZeta = skill['isZeta']
-            if skill['omicronMode'] != 1:
-                isOmicron = True
-                omiMode = skill['omicronMode']
-            else:
-                isOmicron = False
-                omiMode = None
-            for ability in abilities:
-                if ability['id'] != skill['abilityReference']:
-                    continue
-                name = localization[ability['nameKey']]
-                descKey = ability['tier'][-1]['descKey']
-                desc = localization[descKey]
-        return name, desc, maxLevel, isZeta, isOmicron, omiMode
+    def __get_skill_data(self, id, skills, abilities):
+        skill = skills[id]
+        maxLevel = len(skill['tier']) + 1
+        isZeta = skill['isZeta']
+        isOmicron = skill['omicronMode'] != 1
+        omiMode = skill['omicronMode'] if isOmicron else None
+
+        ability = abilities[skill['abilityReference']]
+        name = self.localization[ability['nameKey']]
+        descKey = ability['tier'][-1]['descKey']
+        desc = self.localization[descKey]
+        return id, name, desc, maxLevel, isZeta, isOmicron, omiMode
