966d00715ed7d6169bbee18acdba2cb04fd5be42
Benzi <benzi.villaruel@gmail.com>
2024-11-28 19:10:39 -0500

    Add upcoming events command

--- cogs/events.py
+++ cogs/events.py
@@ -5,7 +5,7 @@ import re
 from datetime import datetime
 import discord
 from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 from titlecase import titlecase
 
 comlink = helpers.comlink
@@ -28,18 +28,18 @@ class Events(commands.Cog):
 
     @events.command(name="current")
     @app_commands.checks.cooldown(1,60)
-    async def current(self, interaction: discord.Interaction, list: bool = True):
+    async def current(self, interaction: discord.Interaction, embeds: bool = False):
         """
         Get all current game events
 
         Args:
-            embeds (bool, optional): Show the events in pages of embeds rather than a list
+            embeds (bool): Show the events in pages of embeds rather than a list (Default: False)
         """
         await interaction.response.send_message("Fetching events...")
         currentTime = int(datetime.now().timestamp() * 1000)
         events = [e for e in helpers.get_events() if currentTime >= e['startTime'] and currentTime < e['endTime']]
         events = sorted(events, key=lambda e: e['endTime'])
-        if list:
+        if not embeds:
             embed = discord.Embed(title="Currently Running Events")
             for event in events:
                 title, subtitle = event['name'].split('\\n')
@@ -65,6 +65,48 @@ class Events(commands.Cog):
     
     @current.error
     async def current_error(self, interaction: discord.Interaction, error):
+        if isinstance(error, app_commands.CommandOnCooldown):
+            await interaction.response.send_message(f"Command is on cooldown for {round(error.retry_after)} seconds", ephemeral=True)
+    
+    @events.command(name="upcoming")
+    @app_commands.checks.cooldown(1,60)
+    async def upcoming(self, interaction: discord.Interaction, embeds: bool = False):
+        """
+        Get all upcoming game events
+
+        Args:
+            embeds (bool): Show the events in pages of embeds rather than a list (Default: False)
+        """
+        await interaction.response.send_message("Fetching events...")
+        currentTime = int(datetime.now().timestamp() * 1000)
+        events = [e for e in helpers.get_events() if currentTime < e['startTime']]
+        events = sorted(events, key=lambda e: e['endTime'])
+        if not embeds:
+            embed = discord.Embed(title="Upcoming Events")
+            for event in events:
+                title, subtitle = event['name'].split('\\n')
+                subtitle = re.sub(r'\[c\]\[.*?\]|\[-\]\[/c\]', '', subtitle)
+                desc = event['desc']
+                embed.add_field(name=f"{self.capitalize_title(title)} - {subtitle}",
+                                value=f"Starts <t:{event['startTime']//1000}:R>",
+                                inline=False
+                )
+            await interaction.edit_original_response(content=None, embed=embed)
+        else:
+            embeds = []
+            for event in events:
+                title, subtitle = event['name'].split('\\n')
+                subtitle = re.sub(r'\[c\]\[.*?\]|\[-\]\[/c\]', '', subtitle)
+                desc = event['desc']
+                embed = discord.Embed(title=f"{self.capitalize_title(title)}\n{subtitle}", description=desc)
+                embed.add_field(name="Start Time", value=f"<t:{event['startTime']//1000}>")
+                embed.add_field(name="End Time", value=f"<t:{event['endTime']//1000}>")
+                embed.set_image(url=f"https://game-assets.swgoh.gg/textures/{event['image']}.png")
+                embeds.append(embed)
+            await interaction.edit_original_response(content=None, embed=embeds[0], view=helpers.EmbedPages(embeds))
+    
+    @upcoming.error
+    async def upcoming_error(self, interaction: discord.Interaction, error):
         if isinstance(error, app_commands.CommandOnCooldown):
             await interaction.response.send_message(f"Command is on cooldown for {round(error.retry_after)} seconds", ephemeral=True)
 
